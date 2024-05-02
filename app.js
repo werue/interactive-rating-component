57ee9b900eed48809552485d8c550a2fb844f24f
W. E. Rue <rue.wev@protonmail.com>
2024-05-01 19:21:08 -0700

    Added event listener that stores the value of radio selection

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const submitBtn = document.querySelector(".submitBtn");
-const yourRating = document.querySelector("youSelected__rating");
+const ratingForm = document.querySelector(".ratingForm");
+const ratingOptions = document.getElementsByName("rating");
+const printedRating = document.querySelector(".youSelected__rating");
 
-//submitBtn.addEventListener()
+let userRating = null;
+
+ratingForm.addEventListener("submit", (e) => {
+    for(option of ratingOptions){
+        if(option.checked){
+            userRating = (option.value);
+        }
+    }
+
+    console.log(userRating);
+    e.preventDefault();
+})
